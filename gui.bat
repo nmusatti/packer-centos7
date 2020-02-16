0b8a6a6ae9e36c284cf7a933a9d6a1e22a227582
Nicola Musatti <nicola.musatti@gmail.com>
2020-02-16 14:47:03 +0100

    Corrected the .bat scripts

--- gui.bat
+++ gui.bat
@@ -1,3 +1,3 @@
-rmdir /s centos7-gui
-IF %ERRORLEVEL% EQU 0 packer.io build centos-gui.json
-IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos7-gui centos7-gui.box
+rmdir /s /q centos7-gui
+IF %ERRORLEVEL% EQU 0 packer build centos-gui.json
+IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos7-gui centos7-gui/package.box
--- mate.bat
+++ mate.bat
@@ -1,3 +1,3 @@
-rmdir /s centos7-mate
-IF %ERRORLEVEL% EQU 0 packer.io build centos-mate.json
-IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos7-mate centos7-mate.box
+rmdir /s /q centos7-mate
+IF %ERRORLEVEL% EQU 0 packer build centos-mate.json
+IF %ERRORLEVEL% EQU 0 vagrant box add -f --name centos7-mate centos7-mate/package.box
